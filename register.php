ac0bd337cc5b6304a4e757a1d3e4f08cf2cff69f
CrimsonPrince <CrimsonPrince@protonmail.com>
2017-11-24 01:49:26 +0000

    More DB code added

--- register.php
+++ register.php
@@ -27,4 +27,44 @@ if(strlen($phone) < 10)
 	$phoneErr = 10;
 		header("Location:index.php?nameErr=" . $nameErr . "&" . "phoneErr=". $phoneErr . "&" . "pwErr=" . $pwErr);
 }
+
+$servername = "localhost";
+$username = "root";
+$passwordDB = "";
+$dbname = "webdevproj";
+
+// Create connection
+$conn = new mysqli($servername,$username,$passwordDB,$dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$result = mysqli_query($conn,"SELECT Username FROM users");
+
+while($row = mysqli_fetch_array($result))
+{	
+	echo "hello";
+	if($name == $row['Username'])
+	{
+		$nameErr = 10;
+		header("Location:index.php?nameErr=" . $nameErr . "&" . "phoneErr=". $phoneErr . "&" . "pwErr=" . $pwErr);
+	}
+}
+
+$sql = "INSERT INTO `users`(`Username`, `Password`, `Email`, `Phone`)
+VALUES ('$name','$password','$email','$phone')";
+
+if ($conn->query($sql) === TRUE) 
+{
+   header("Location:User.php");
+} 
+else 
+{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+
 ?>
