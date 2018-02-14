6394d0b1f947c48f2b5974a298ba7e38907d906c
Unknown <Kingr4@protonmail.ch>
2018-02-14 17:42:26 +0000

    Added Main method

--- ie.dit/main.java
+++ ie.dit/main.java
@@ -1,77 +1,8 @@
-package ie.dit;
-import java.util.ArrayList;
-
-ArrayList<Tune> tunes = new ArrayList<Tune>();
-
-public class TuneBook {
-
-    public TuneBook(String Filename)
-    {
-        this.Filename = Filename;
-
-
-        BufferedReader inputStream = null;
-        try {
-            inputStream = new BufferedReader(new FileReader(Filename));
-
-            String l, s;
-            while ((l = inputStream.readLine()) != null) {
-                if(l.substring(0,2) == "X:")
-                {
-                    Tune tune = new Tune();
-                    tune.setX(substring(3,3));
-
-
-                if(!l.startsWith("X:")) {
-                    //Count Variable used to keep track of whether or not the alt titles have been entered
-                    int count = 0;
-                    if(l.startsWith("T:") && count == 0)
-                    {
-                        //Sets count to one indicating that main title was recorded
-                        tune.setTitle(l)
-                        count = 1;
-                    }
-                    //Checking for if main title is recorded and a second Title has appeared
-                    if(l.startsWith("T:") && count == 1)
-                    {
-                        tune.setaltTitle(l);
-                        count = 2;
-                        //Sets count to 2, to prevent altTitle from being set to null below
-                    }
-                    if(count = 1) {
-                        tune.setaltTitle(null);
-
-                    }
-
-                    if(l.startsWith("K:"))
-                    {
-                    s += s + l;
-                    }
-
-                }
-                    tune.setNotation(s);
-                    s = null;
-                    this.tunes.add(temp);
-                }
-            }
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            if (inputStream != null) {
-                try
-                {
-                    inputStream.close();
-                }
-                catch(Exception e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+public class main {
 
 }
+
+public static void main(String[] args){
+        TuneBook tb = new TuneBook("hnj0.abc");
+        System.out.println(tb);
+        }
