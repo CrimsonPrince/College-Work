2dd63c368f6292ab5886d94ad53751e912c915e9
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-09-30 17:55:36 +0100

    Got Deletion Working

--- Year-4/Rich-Web/Lab1/Problem-3/Problem-3.js
+++ Year-4/Rich-Web/Lab1/Problem-3/Problem-3.js
@@ -1,3 +1,6 @@
+var deleteIcons = document.getElementsByClassName("deleteIcon");
+var editIcons = document.getElementsByClassName("editIcon");
+
 function addItem() {
 
     let input = document.getElementById("newItemInput").value;
@@ -11,15 +14,44 @@ function addItem() {
 
   let list = document.getElementById("notesList")
   let li = document.createElement("li");
-  let span = document.createElement("span");
-  let icon = document.createElement("i");
-
-  icon.classList.add("fas", "fa-trash")
+  let deleteSpan = document.createElement("span");
+  let deleteIcon = document.createElement("i");
+  let editSpan = document.createElement("span");
+  let editIcon = document.createElement("i");
 
   li.classList.add("noteItem");
 
-  list.appendChild(li).appendChild(span, input);
+  deleteIcon.classList.add("fas", "fa-trash")
+  deleteSpan.appendChild(deleteIcon);
+  deleteSpan.classList.add("deleteIcon");
 
+  editIcon.classList.add("fas", "fa-edit")
+  editSpan.appendChild(editIcon);
+  editSpan.classList.add("editIcon");
+
+  list.appendChild(li).append(input, deleteSpan, editSpan);
+
+  console.log("Created Note with Value", input)
+
+  addListeners();
+}
+
+
+function addListeners() {
+  
+  for(let span of deleteIcons){
+    span.addEventListener ("click",function (){
+      span.parentElement.remove();
+      event.stopPropagation();
+    });
+  }
+
+  for(let span of editIcons){
+    span.addEventListener ("click",function (){
+      span.parentElement.addItem;
+      event.stopPropagation();
+    });
+  }
 }
 
 function deleteItem(value)
