d08e38eb449192f26e69df43cf1b0b5c67739eb4
crimsonprince <kingr4@protonmail.ch>
2019-10-01 19:15:49 +0100

    Added Calculator and Gitignore

--- Year-4/Rich-Web/Lab1/Problem-3/Problem-3.js
+++ Year-4/Rich-Web/Lab1/Problem-3/Problem-3.js
@@ -1,48 +1,9 @@
-var deleteIcons = document.getElementsByClassName("deleteIcon");
-var editIcons = document.getElementsByClassName("editIcon");
-
-function addItem() {
-
-    let input = document.getElementById("newItemInput").value;
-
-    if (input === '') {
-        alert("Text Field Cannot Be Empty");
-        return;
-      }
-    document.getElementById("newItemInput").value = "";
-
-
-  let list = document.getElementById("notesList")
-  let li = document.createElement("li");
-  let deleteSpan = document.createElement("span");
-  let deleteIcon = document.createElement("i");
-  let editSpan = document.createElement("span");
-  let editIcon = document.createElement("i");
-
-  li.classList.add("noteItem");
-
-  deleteIcon.classList.add("fas", "fa-trash")
-  deleteSpan.appendChild(deleteIcon);
-  deleteSpan.classList.add("deleteIcon");
-
-  editIcon.classList.add("fas", "fa-edit")
-  editSpan.appendChild(editIcon);
-  editSpan.classList.add("editIcon");
-
-  list.appendChild(li).append(input, deleteSpan, editSpan);
-
-  console.log("Created Note with Value", input)
-
-  addListeners();
-}
-
-
 function addListeners() {
   
   spans = Array.from(document.getElementsByClassName("noteItem"));
   for(let [i, span] of spans.entries() ){
     span.addEventListener("click", function() {
-      deleteNote(i)
+      editNote(i)
     }
     );
   }
@@ -109,4 +70,11 @@ function retrieveNotes(){
   }
 
   return notes;
+}
+
+function editNote(i)
+{
+  let liElement = document.getElementsByTagName("li");
+  let element = liElement[i];
+  element = document.createElement("input");
 }
