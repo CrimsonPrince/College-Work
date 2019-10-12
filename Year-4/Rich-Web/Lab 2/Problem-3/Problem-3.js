75ee5ca02042e0894eb09845afa12b852bf0e57c
crimsonprince <kingr4@protonmail.ch>
2019-10-12 15:37:23 +0100

    Implemented Problem 3

--- Year-4/Rich-Web/Lab 2/Problem-3/Problem-3.js
+++ Year-4/Rich-Web/Lab 2/Problem-3/Problem-3.js
@@ -1,24 +1,44 @@
-async function getUserAsync(name) {
-    try{
-      let response = await fetch(`https://api.github.com/users/${name}`);
-      return await response.json();
-    }catch(err){
-      console.error(err);
-    }
-  }
+let token = "2f7a36c0a4db21f9bf7f25de5d4d00988edbbba7";
+let headers = { "Content-Type": "application/json", "Authorization": `Token ${token}` };
 
-async function getRepositoryAsync(name)
-{
-    try{
-        let response = await fetch(`https://api.github.com/users/${name}/repos`);
+async function getUserAsync(name) {
+    try {
+        let response = await fetch(`https://api.github.com/users/${name}`, { headers, });
         return await response.json();
-      }catch(err){
+    } catch (err) {
         console.error(err);
-      }
+    }
 }
 
-  getUserAsync('CrimsonPrince')
-  .then(data => console.log(data) );
+async function getRepositoryAsync(name) {
+    try {
+        let response = await fetch(`https://api.github.com/users/${name}/repos`);
+        return await response.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-  getRepositoryAsync('CrimsonPrince')
-  .then(datas => console.log(datas) );
+function displayUser(data) {
+    document.querySelector('#avatar').src = data.avatar_url;
+    document.querySelector('#name').innerHTML = data.name;
+    document.querySelector('#username').innerHTML = data.login;
+    document.querySelector('#emailDiv').innerHTML = data.email;
+    document.querySelector('#location').innerHTML = data.location;
+    document.querySelector('#gists').innerHTML = data.public_gists;
+}
+
+function displayRepos(data) {
+    let repoInfo = document.querySelector("#repoInfo");
+    data.forEach(function(repo) {
+        let repository = document.createElement("div");
+        repository.innerHTML = repo.name;
+        repoInfo.appendChild(repository);
+    });
+}
+
+getUserAsync('scottdono')
+    .then(data => displayUser(data));
+
+getRepositoryAsync('scottdono')
+    .then(datas => displayRepos(datas));
