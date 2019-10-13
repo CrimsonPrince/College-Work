3990dafe6bea996f5b419cd1b53988771b49b565
crimsonprince <kingr4@protonmail.ch>
2019-10-13 14:39:19 +0100

    Ran Formatter, Added Searching

--- Year-4/Rich-Web/Lab 2/Problem-1/Problem-1.js
+++ Year-4/Rich-Web/Lab 2/Problem-1/Problem-1.js
@@ -1,4 +1,4 @@
-let  form = document.querySelector('form');
+let form = document.querySelector('form');
 let name = document.querySelector('#name');
 let number = document.querySelector('#number');
 let email = document.querySelector('#email');
@@ -8,84 +8,83 @@ let sorted = -1;
 let searchBar = document.querySelector("#search");
 
 class Contact {
-    constructor() {
-        this.name = name.value;
-        this.number = number.value;
-        this.email = email.value;
-        name.value = "";
-        number.value = "";
-        email.value = "";
-    }
+  constructor() {
+    this.name = name.value;
+    this.number = number.value;
+    this.email = email.value;
+    name.value = "";
+    number.value = "";
+    email.value = "";
+  }
 }
 
 
-form.addEventListener('submit', function(e) {
-    addContact();
-  })
-
-searchBar.addEventListener('keyup', function(e) { 
-    search();
+form.addEventListener('submit', function (e) {
+  addContact();
 })
 
-function sortTable()
-{
-    contacts = retrieveContacts();
-    contacts.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()) * sorted);
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-    sorted = (sorted == -1)? 1 : -1; 
-    displayTable();
+searchBar.addEventListener('keyup', function (e) {
+  search();
+})
+
+function sortTable() {
+  contacts = retrieveContacts();
+  contacts.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()) * sorted);
+  localStorage.setItem("contacts", JSON.stringify(contacts));
+  sorted = (sorted == -1) ? 1 : -1;
+  displayTable();
 }
 
 function displayTable() {
-    tableBody.innerHTML = "";
-    let contacts = retrieveContacts();
-    contacts.forEach(element => {
-        tableBody.appendChild(toTableRow(element));
-    });
+  tableBody.innerHTML = "";
+  let contacts = retrieveContacts();
+  contacts.forEach(element => {
+    tableBody.appendChild(toTableRow(element));
+  });
 }
 
 function addContact() {
-    validateInput();
-    let contact = new Contact();
-    let contacts = retrieveContacts();
-    contacts.push(contact)
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-    displayTable();
+  validateInput();
+  let contact = new Contact();
+  let contacts = retrieveContacts();
+  contacts.push(contact)
+  localStorage.setItem("contacts", JSON.stringify(contacts));
+  displayTable();
 }
 
 function retrieveContacts() {
-    let contacts = JSON.parse(localStorage.getItem("contacts"));
-    if( contacts === null) { 
-        contacts = [];
-    }
-    return contacts;
+  let contacts = JSON.parse(localStorage.getItem("contacts"));
+  if (contacts === null) {
+    contacts = [];
+  }
+  return contacts;
 }
 
 function toTableRow(contact) {
-    let row = document.createElement("tr");
-    let nameCol = document.createElement("td");
-    let numberCol = document.createElement("td");
-    let emailCol = document.createElement("td");
-    nameCol.innerHTML = contact.name;
-    numberCol.innerHTML = contact.number;
-    emailCol.innerHTML = contact.email;
-    row.append(nameCol, numberCol, emailCol);
-    return row;
+  let row = document.createElement("tr");
+  let nameCol = document.createElement("td");
+  let numberCol = document.createElement("td");
+  let emailCol = document.createElement("td");
+  nameCol.innerHTML = contact.name;
+  numberCol.innerHTML = contact.number;
+  emailCol.innerHTML = contact.email;
+  row.append(nameCol, numberCol, emailCol);
+  return row;
 }
 
 function search() {
-    let tr, td;
-    tr = table.querySelectorAll("tr");
-    for (let i = 0; i < tr.length; i++) {
-      td = tr[i].querySelectorAll("td")[1];
-      if (td) {
-        if (td.textContent.indexOf(searchBar.value) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
+  let tr, td;
+  tr = table.querySelectorAll("tr");
+  for (let i = 0; i < tr.length; i++) {
+    td = tr[i].querySelectorAll("td")[1];
+    if (td) {
+      if (td.textContent.indexOf(searchBar.value) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
       }
     }
   }
+}
 
 displayTable();
