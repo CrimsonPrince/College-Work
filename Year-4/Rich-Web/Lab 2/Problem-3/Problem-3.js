8da152bbe71d4d126cbfa82262f5be33fde5a33c
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-10-15 10:55:05 +0100

    Upgraded Styling on Problem 3

--- Year-4/Rich-Web/Lab 2/Problem-3/Problem-3.js
+++ Year-4/Rich-Web/Lab 2/Problem-3/Problem-3.js
@@ -33,13 +33,14 @@ function displayUser(data) {
     document.querySelector('#avatar').src = data.avatar_url;
     document.querySelector('#name').innerHTML = data.name;
     document.querySelector('#username').innerHTML = data.login;
-    document.querySelector('#emailDiv').innerHTML = data.email;
+    document.querySelector('#email').innerHTML = data.email;
     document.querySelector('#location').innerHTML = data.location;
     document.querySelector('#gists').innerHTML = data.public_gists;
 }
 
 function displayRepositories(data) {
     let repoInfo = document.querySelector("#repoInfo");
+    repoInfo.innerHTML = "";
     data.forEach(function (repo) {
         let repository = document.createElement("div");
         repository.innerHTML = repo.name;
