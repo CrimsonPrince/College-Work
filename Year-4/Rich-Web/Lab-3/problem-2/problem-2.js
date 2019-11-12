89d99edfdda85b8574c166b40868085b1bae89b4
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-11-12 15:20:45 +0000

    Started Problem 2 RichWeb
    Signed-off-by: crimsonprince <kingr4@protonmail.ch>

--- Year-4/Rich-Web/Lab-3/problem-2/problem-2.js
+++ Year-4/Rich-Web/Lab-3/problem-2/problem-2.js
@@ -1,59 +1,28 @@
-(function () {
-  const list = document.querySelector('#list')
-  const item = document.querySelector('#item')
+startButton = document.querySelector('#start')
+Rx.Observable.fromEvent(startButton, 'click')
+  .subscribe(e => start())
 
-  Rx.Observable.fromEvent(item, 'keydown')
-    .filter(event => event.keyCode === 13)
-    .subscribe(e => addNote(e))
+function start () {
+  const hour = document.querySelector('#hour')
+  const minute = document.querySelector('#minute')
+  const second = document.querySelector('#second')
 
-  function store () {
-    window.localStorage.myitems = list.innerHTML
-    getValues()
-  }
+  console.log(hour.value)
+  console.log(minute.value)
+  console.log(second.value)
+  const seconds = Number(hour.value) * 60 * 60 + Number(minute.value) * 60 + Number(second.value)
+  console.log(seconds)
+  var source = Rx.Observable.timer(0, 1000)
+    .take(seconds)
 
-  function getValues () {
-    var storedValues = window.localStorage.myitems
-    if (!storedValues) {
-      list.innerHTML = ''
-    } else {
-      list.innerHTML = storedValues
-    }
-    addObservables()
-  }
-
-  function addObservables () {
-    const editButtons = document.querySelectorAll('.editButton')
-    const deleteButtons = document.querySelectorAll('.deleteButton')
-    Rx.Observable.fromEvent(deleteButtons, 'click')
-      .subscribe(e => removeNote(e))
-
-    Rx.Observable.fromEvent(editButtons, 'click')
-      .subscribe(e => editNote(e))
-  }
-
-  function addNote (e) {
-    if (item.value === '') {
-      return
-    }
-    e.preventDefault()
-    list.innerHTML += '<li>' + item.value + '<input type="button" class="editButton" value="Edit">' + '<input type="button" class="deleteButton" value="Delete">' + '</li>'
-    store()
-    item.value = ''
-  }
-
-  function removeNote (e) {
-    console.log(e)
-    var t = e.target
-    t = t.parentNode
-    t.parentNode.removeChild(t)
-    store()
-  }
-
-  function editNote (e) {
-    Rx.Observable.fromEvent(document, 'submit')
-      .subscribe(store())
-  }
-
-  getValues()
+  var subscription = source.subscribe(
+    function (x) {
+      console.log('Next: ' + x)
+    },
+    function (err) {
+      console.log('Error: ' + err)
+    },
+    function () {
+      console.log('Completed')
+    })
 }
-)()
