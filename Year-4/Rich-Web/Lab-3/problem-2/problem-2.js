2bdff84951ea790aa5b69d111d17e7f8500e20a7
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-11-20 15:56:14 +0000

    Final Submission Rich Web Lab 3
    Signed-off-by: crimsonprince <kingr4@protonmail.ch>

--- Year-4/Rich-Web/Lab-3/problem-2/problem-2.js
+++ Year-4/Rich-Web/Lab-3/problem-2/problem-2.js
@@ -1,15 +1,53 @@
+// const startButton = document.querySelector('#start')
+// let toggle = false
+// Rx.Observable.fromEvent(startButton, 'click').subscribe(ev => {
+//   toggle = true
+//   start()
+// })
+
+// function start () {
+//   const hour = document.querySelector('#hour')
+//   const minute = document.querySelector('#minute')
+//   const second = document.querySelector('#second')
+//   const secondsDisplay = document.querySelector('#secondsDisplay')
+//   const minutesDisplay = document.querySelector('#minutesDisplay')
+//   const hourDisplay = document.querySelector('#hoursDisplay')
+
+//   let seconds = Number(hour.value) * 60 * 60 + Number(minute.value) * 60 + Number(second.value)
+
+//   const stream = Rx.Observable
+//     .interval(1000)
+//     .take(seconds)
+
+//   const subs = stream.subscribe(t => {
+//   // if the start button has not been clicked no increment required
+//     if (toggle === false) return
+
+//     if (seconds === 0) subs.unsubscribe()
+
+//     seconds = seconds - 1
+//     const hours = Math.floor(seconds / 3600)
+//     const minutes = Math.floor(seconds % 3600 / 60)
+//     const secondsRem = Math.floor(seconds % 3600 % 60)
+//     hourDisplay.innerHTML = hours
+//     minutesDisplay.innerHTML = minutes
+//     secondsDisplay.innerHTML = secondsRem
+//     console.log('Hours: ' + hours + ' Minutes: ' + minutes + ' Seconds: ' + secondsRem)
+//   },
+//   function (err) {
+//     console.log('Error: ' + err)
+//   },
+//   function () {
+//     console.log('Completed')
+//   })
+// }
+
 const startButton = document.querySelector('#start')
-const toggle = Rx.Observable.fromEvent(startButton, 'click')
+Rx.Observable.fromEvent(startButton, 'click')
+  .subscribe(e => start())
 
 Rx.Observable.fromEvent(document, 'submit')
   .subscribe(e => start())
-
-toggle
-  .subscribe(
-    () => { start() },
-    (error) => console.error(error),
-    () => console.log('Click listener completed')
-  )
 
 function start () {
   const hour = document.querySelector('#hour')
@@ -23,7 +61,6 @@ function start () {
   console.log('Total Seconds: ' + seconds)
   const source = Rx.Observable.timer(0, 1000)
     .take(seconds)
-    .takeUntil(toggle)
 
   source.subscribe(
     function (x) {
