cd97b93c0bdc0e56209c9d4778fc9382507f2bc0
crimsonprince <kingr4@protonmail.ch>
2019-12-03 17:03:43 +0000

    Added Security Lab 4
    Signed-off-by: crimsonprince <kingr4@protonmail.ch>

---
+++ Year-4/Security/Lab-4/rsa.py
@@ -0,0 +1,83 @@
+#Encoding: UTF-8
+import random
+
+def gcd(a, b):
+    '''Return the greatest common divisor (gcd) of two numbers.
+    Use the recursive variant of the extended Euclidean algorithm
+    to compute the GCD of a and b. Also find two numbers x and y
+    that satisfy ax + by = gcd(a,b).
+    '''
+    if b == 0:
+        return (a, 1, 0)
+    (d_, x_, y_) = gcd(b, a % b)
+    (d, x, y) = (d_, y_, x_ - (a // b) * y_)
+    return (d, x, y)
+
+#Fermats primality test used with base 2. This will return reliable prime numbers up to prime<=340.
+def isPrime(number):
+	if ( (2 ** (number -1) ) % number ) == 1:
+		return (True )# and isPrime(number,base-1))
+	else:
+		return False
+
+def gen_prime(max):
+	for i in xrange(max,2,-1):
+		if(isPrime(i)):
+			return i
+
+def gen_privkey(p, q, e):
+    ''' Return the private key exponent d.
+    Compute the private exponent d using the extended Euclidean algorithm.
+    d is the multiplicative inverse of e % ((p - 1) * (q - 1)),
+    so it satisfies d * e == 1 % ((p - 1) * (q - 1)).
+    '''
+    phi_n = (p - 1) * (q - 1)
+    (x, d, y) = gcd(e, phi_n)
+    if d < 0: d += phi_n
+    return d
+
+def pubkey_encrypt(text, pubkey, factor):
+	return text ** pubkey % factor
+
+def privkey_decrypt(chiffre, privkey, factor):
+	return chiffre **  privkey % factor
+
+print "Generating prime p"
+prime_p = gen_prime(random.randint(10,50))
+print ">>Prime p:" + str(prime_p)
+
+print "Generating prime q"
+prime_q = gen_prime(random.randint(10,50))
+print ">>Prime q:" + str(prime_q)
+
+assert prime_p != prime_q
+
+print "Calculating factor n"
+factor_n = prime_p*prime_q
+print ">>Factor n:" + str(factor_n)
+
+print "Calculating Phi(N)"
+phi_n = (prime_p-1)*(prime_q-1)
+print ">>Phi(N):" + str(phi_n)
+
+# e doesn't have to be choosen at random. any prime number < phi_n will work.
+e = 17
+print ">>Public exponent e:" + str(e)
+
+assert e < phi_n
+
+print "Generating pubkey d"
+privkey_d = gen_privkey(prime_p, prime_q, e)
+print ">>Privkey d:" + str(privkey_d)
+
+print "Generating text"
+text_clear = random.randint(0,100)
+print ">>Text:" + str(text_clear)
+
+print "Encrypting"
+text_encrypted = pubkey_encrypt(text_clear, e, factor_n)
+print ">>Text (encrypted):" + str(text_encrypted)
+
+print "Decrypting"
+text_decrypted = privkey_decrypt(text_encrypted, privkey_d, factor_n)
+print ">>Text (decrypted):" + str(text_decrypted)
