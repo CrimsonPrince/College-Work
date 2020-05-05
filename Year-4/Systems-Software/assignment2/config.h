28730515048fed4d3c95ff303affc66c5e1e4fe7
r4 <kingr4@protonmail.ch>
2020-05-05 16:57:14 +0100

    Breakpoint
    Signed-off-by: r4 <kingr4@protonmail.ch>

--- Year-4/Systems-Software/assignment2/config.h
+++ Year-4/Systems-Software/assignment2/config.h
@@ -1,3 +1,5 @@
+#define MAX_BUFFER 1024
 #define MAX_FILEPATH_LENGTH 260
+#define BASE_PATH "/var/www/intranet/html"
 #define PORT 8000
-#define ADDRESS "136.243.125.52"
+#define ADDRESS "127.0.0.1"
